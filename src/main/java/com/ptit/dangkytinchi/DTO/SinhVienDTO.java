6e3b77a374188f938fd618da77d5e1d29c631572
tiendatc2mh <tiendatc2mh@gmail.com>
2022-04-17 22:02:20 +0700

    jnuit SinhVienDTO

--- src/main/java/com/ptit/dangkytinchi/DTO/SinhVienDTO.java
+++ src/main/java/com/ptit/dangkytinchi/DTO/SinhVienDTO.java
@@ -11,5 +11,40 @@ public class SinhVienDTO implements Serializable {
     private String taiKhoan;
     private String matKhau;
     private String matKhauMoi;
-    private  String lop;
+    private String lop;
+
+    public SinhVienDTO() {
+    }
+
+    public SinhVienDTO(String taiKhoan, String matKhau) {
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+    }
+
+    public SinhVienDTO(String maSinhVien, String tenSinhVien, String taiKhoan, String matKhau, String matKhauMoi, String lop) {
+        this.maSinhVien = maSinhVien;
+        this.tenSinhVien = tenSinhVien;
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+        this.matKhauMoi = matKhauMoi;
+        this.lop = lop;
+    }
+
+    public SinhVienDTO(String taiKhoan, String matKhau, String matKhauMoi) {
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+        this.matKhauMoi = matKhauMoi;
+    }
+
+    @Override
+    public String toString() {
+        return "SinhVienDTO{" +
+                "maSinhVien='" + maSinhVien + '\'' +
+                ", tenSinhVien='" + tenSinhVien + '\'' +
+                ", taiKhoan='" + taiKhoan + '\'' +
+                ", matKhau='" + matKhau + '\'' +
+                ", matKhauMoi='" + matKhauMoi + '\'' +
+                ", lop='" + lop + '\'' +
+                '}';
+    }
 }
